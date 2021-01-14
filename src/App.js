a7d63d0abe3ef35c0159ab29ce4f3a3278c5b376
XolaniMgube <xnmgube@gmail.com>
2021-01-14 13:56:55 +0200

    ended on textfields

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+// In order to use the core materials, you need to install the core package first
+// npm install @material-ui/core
+
+// To you the material ui icons, you have to install the icons package
+// npm install @material-ui/icons
+
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import TextField from '@material-ui/core/TextField'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Xolani</h1>
+
+      {/* Using Buttons ***********************************************************/}
+      <Button
+        startIcon={<SaveIcon />}
+        size="large"
+        disabled
+        onClick={() => alert("Hey bro")}
+        href="http://www.xolanimgube.co.za/"
+        variant="contained"
+        color="primary"
+        style={{
+          fontSize: "14px",
+        }}
+      >
+        Hello World
+      </Button>
+
+      {/* Buttons with Icons ***********************************************8*/}
+      <Button color="secondary" variant="contained" endIcon={<SaveIcon />}>
+        Xolani
+      </Button>
+
+      {/* Button Groups ***************************************************/}
+      <div>
+        <ButtonGroup>
+          <Button startIcon={<SaveIcon />} color="primary">
+            Save
+          </Button>
+          <Button color="secondary" endIcon={<DeleteIcon />}>
+            Discard
+          </Button>
+        </ButtonGroup>
+      </div>
+
+      {/*CheckBox and Labels ***************************************************/}
+
+      <FormControlLabel
+        control={
+          <Checkbox
+            icon={<SaveIcon />}
+            inputProps={{
+              "aria-label": "primary checkbox",
+            }}
+          ></Checkbox>
+        }
+        label="Testing Label"
+      />
+
+      {/* TextFields ********************************************************/}
+      <div>
+        <TextField 
+          variant="filled"
+          color="secondary"
+          type="email"
+          label="Input email here"
+          placeholder="email@email.com"
+        />
+
+        <TextField 
+          variant="outlined"
+          type="date"
+        />
+
+      </div>
     </div>
   );
 }
